de7c9774980425564ed9f19c4c3bca24619621d5
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2026-02-18 07:13:01 +0000

    rm proto

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=proto/market.proto");
-
-    tonic_build::configure()
-        .build_server(true)
-        .compile(&["proto/market.proto"], &["proto"])?;
-
-    Ok(())
-}
